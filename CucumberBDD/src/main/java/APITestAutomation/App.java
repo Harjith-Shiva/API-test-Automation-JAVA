cf2afc5ff9ccc541155bc8439659415fd864b7a5
Harjith Shiva <harjith.shiva@gmail.com>
2024-03-13 10:34:39 +0200

    Cucumber API test Automation

---
+++ CucumberBDD/src/main/java/APITestAutomation/App.java
@@ -0,0 +1,13 @@
+package APITestAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
